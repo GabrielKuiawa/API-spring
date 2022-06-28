65bb2aa65644cb14252a40c8582e085d83f4dc44
Alan Gabriel <gabrielsantos77339@gmail.com>
2022-06-27 23:05:33 -0300

    primeio_commit

---
+++ projeo_spring/src/test/java/com/example/projeo_spring/ProjeoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
