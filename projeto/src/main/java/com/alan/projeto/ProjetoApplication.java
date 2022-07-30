533a240a8dfd4cf1a8fd19cbfd266812214575d1
Alan Gabriel <gabrielsantos77339@gmail.com>
2022-07-30 20:48:37 -0300

    salvando e recuperando cliete

--- projeto/src/main/java/com/alan/projeto/ProjetoApplication.java
+++ projeto/src/main/java/com/alan/projeto/ProjetoApplication.java
@@ -1,11 +1,30 @@
 package com.alan.projeto;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.alan.projeto.domain.entity.Cliente;
+import com.alan.projeto.domain.repositorio.Clientes;
 
 @SpringBootApplication
 public class ProjetoApplication {
 
+	@Bean
+	public CommandLineRunner init( @Autowired Clientes clientes){
+		return args -> {
+			clientes.salvar(new Cliente("Alan Gabriel"));
+			clientes.salvar(new Cliente("Alan Douglas"));
+			
+			List<Cliente> todosClientes = clientes.listar();
+			todosClientes.forEach(System.out::println);
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
 	}
