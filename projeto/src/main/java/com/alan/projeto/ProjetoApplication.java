13542923ed215013fd98bae7da900e32f2a27ab6
Alan Gabriel <gabrielsantos77339@gmail.com>
2022-07-30 21:45:22 -0300

    concluindo cadastro de clientes

--- projeto/src/main/java/com/alan/projeto/ProjetoApplication.java
+++ projeto/src/main/java/com/alan/projeto/ProjetoApplication.java
@@ -17,11 +17,37 @@ public class ProjetoApplication {
 	@Bean
 	public CommandLineRunner init( @Autowired Clientes clientes){
 		return args -> {
+			System.out.println("\n Salvando clientes :");
 			clientes.salvar(new Cliente("Alan Gabriel"));
 			clientes.salvar(new Cliente("Alan Douglas"));
 			
 			List<Cliente> todosClientes = clientes.listar();
 			todosClientes.forEach(System.out::println);
+
+			System.out.println("\n Atualizando clientes:");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " Atualizado");
+				clientes.atualizar(c);
+			});
+
+			todosClientes.forEach(System.out::println);
+
+			System.out.println("\n Buscando cliente :");
+			clientes.buscarPorNome("D").forEach(System.out::println);
+			todosClientes = clientes.listar();
+
+			System.out.println("\n Deletando clientes :");
+			clientes.listar().forEach(c -> {
+				System.out.println(c);
+				clientes.deletar(c);				
+			});	
+			
+			todosClientes = clientes.listar();
+			if (todosClientes.isEmpty()){
+				System.out.println("\n nenhum cliente encontrado");
+			} else {
+				todosClientes.forEach(System.out::println);
+			}
 		};
 	}
 	
--- projeto/src/main/java/com/alan/projeto/domain/repositorio/Clientes.java
+++ projeto/src/main/java/com/alan/projeto/domain/repositorio/Clientes.java
@@ -16,6 +16,9 @@ public class Clientes {
 
     private static String INSERT = "insert into cliente (nome) values (?)";
     private static String SELECT_ALL = "select * from cliente";
+    private static String SELECT_POR_NOME = "select * from cliente where nome = ?";
+    private static String UPDATE = "update cliente set nome = ? where id = ?";
+    private static String DELETE = "delete from cliente where id = ?";
     
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -25,8 +28,33 @@ public class Clientes {
         return cliente;
     }
     
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[] {
+            cliente.getNome(),
+            cliente.getId()
+        });
+        return cliente;
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[] {id});
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public List<Cliente> buscarPorNome(String nome) {
+        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "),
+        new Object[] {"%" + nome + "%"}, ObterCliente());
+    }
+
     public List<Cliente> listar() {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, ObterCliente());
+    }
+
+    private RowMapper<Cliente> ObterCliente() {
+        return new RowMapper<Cliente>() {
 
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
@@ -35,6 +63,6 @@ public class Clientes {
                 return new Cliente(id, nome);
             }
             
-        });
+        };
     }
 }
