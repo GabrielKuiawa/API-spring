5c8aed540ac6a2e53d8d7d2a7e663169c84adae4
Alan Gabriel <gabrielsantos77339@gmail.com>
2022-08-06 14:12:56 -0300

    mapeando a Entidade Cliente para JPA

--- projeto/src/main/java/com/alan/projeto/domain/entity/Cliente.java
+++ projeto/src/main/java/com/alan/projeto/domain/entity/Cliente.java
@@ -1,7 +1,24 @@
 package com.alan.projeto.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
     public Cliente() {
     }
     
@@ -13,19 +30,19 @@ public class Cliente {
     public Cliente(String nome) {
         this.nome = nome;
     }
-
-    private Integer id;
-    private String nome;
     
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
