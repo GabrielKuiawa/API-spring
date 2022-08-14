db5a75dc7c2b9aa5dcef46d18022a2955bf6671c
Alan Gabriel <gabrielsantos77339@gmail.com>
2022-08-14 15:56:25 -0300

    mapeando entidade cliente jpa

--- projeto/src/main/java/com/alan/projeto/VendasApplication.java
+++ projeto/src/main/java/com/alan/projeto/VendasApplication.java
@@ -18,31 +18,31 @@ public class VendasApplication {
 	public CommandLineRunner init( @Autowired Clientes clientes){
 		return args -> {
 			System.out.println("\n Salvando clientes :");
-			clientes.salvar(new Cliente("Alan Gabriel"));
-			clientes.salvar(new Cliente("Alan Douglas"));
+			clientes.save(new Cliente("Alan Gabriel"));
+			clientes.save(new Cliente("Alan Douglas"));
 			
-			List<Cliente> todosClientes = clientes.listar();
+			List<Cliente> todosClientes = clientes.findAll();
 			todosClientes.forEach(System.out::println);
 
 			System.out.println("\n Atualizando clientes:");
 			todosClientes.forEach(c -> {
 				c.setNome(c.getNome() + " Atualizado");
-				clientes.atualizar(c);
+				clientes.save(c);
 			});
 
 			todosClientes.forEach(System.out::println);
 
 			System.out.println("\n Buscando cliente :");
-			clientes.buscarPorNome("D").forEach(System.out::println);
-			todosClientes = clientes.listar();
+			clientes.encontrarPorNome("D").forEach(System.out::println);
+			todosClientes = clientes.findAll();
 
 			System.out.println("\n Deletando clientes :");
-			clientes.listar().forEach(c -> {
+			clientes.findAll().forEach(c -> {
 				System.out.println(c);
-				clientes.deletar(c);				
+				clientes.delete(c);				
 			});	
 			
-			todosClientes = clientes.listar();
+			todosClientes = clientes.findAll();
 			if (todosClientes.isEmpty()){
 				System.out.println("\n nenhum cliente encontrado");
 			} else {
---
+++ projeto/src/main/java/com/alan/projeto/domain/repositorio/JpaRepository.java
@@ -0,0 +1,5 @@
+package com.alan.projeto.domain.repositorio;
+
+public class JpaRepository<T1, T2> {
+
+}
