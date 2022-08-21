67548bf41db03e3ed7a6aebe94263bc4238a155b
GabrielKuiawa <gabrielsantos77339@gmail.com>
2022-08-21 14:47:47 -0300

    get mapping

--- projeto/src/main/java/com/alan/projeto/VendasApplication.java
+++ projeto/src/main/java/com/alan/projeto/VendasApplication.java
@@ -1,10 +1,23 @@
 package com.alan.projeto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alan.projeto.domain.entity.Cliente;
+import com.alan.projeto.domain.repository.Clientes;
+
 @SpringBootApplication
 public class VendasApplication {
+
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+		return args -> {
+			Cliente c = new Cliente(null,"alan");
+			clientes.save(c);
+		};
+	}
+
 	public static void main(String[] 	args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
