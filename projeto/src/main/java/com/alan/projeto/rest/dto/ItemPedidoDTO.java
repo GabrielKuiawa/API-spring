303832948d135be5017bc6f797cdeafd65704410
GabrielKuiawa <gabrielsantos77239@gmail.com>
2022-09-07 22:50:17 -0300

    Criando DTO de pedidos

---
+++ projeto/src/main/java/com/alan/projeto/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,25 @@
+package com.alan.projeto.rest.dto;
+
+public class ItemPedidoDTO {
+
+    private Integer produto;
+
+    private Integer quantidade;
+
+    public Integer getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Integer produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
